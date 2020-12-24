c1a5b100d49e1a2242f286a0597e391ba782706b
Christopher Guerrero <chrisgrr3@gmail.com>
2020-12-23 20:03:20 -0900

    Update DVDTest.java

--- Intro/DVDTest.java
+++ Intro/DVDTest.java
@@ -16,6 +16,14 @@ public class DVDTest {
         System.out.println(dvdCollection[7]); // The Avengers, directed by Joss Whedon, released in 2012
         System.out.println(dvdCollection[10]); //null
         System.out.println(dvdCollection[3]); //The Incredibles, directed by Brad Bird, released in 2004
+
+        // Writing items into array with a loop
+        int[] squareNumbers = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            int square = (i + 1) * (i + 1);
+            squareNumbers[i] = square;
+        }
     }
 }
 
