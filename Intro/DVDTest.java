6b2a039c56a5b22d51ce86bfe4ccd0e3b94bf8cf
Christopher Guerrero <chrisgrr3@gmail.com>
2020-12-23 19:54:04 -0900

    Update DVDTest.java
    Inlcuded writing into an array and reading the items in the array.

--- Intro/DVDTest.java
+++ Intro/DVDTest.java
@@ -1,7 +1,21 @@
 public class DVDTest {
     public static void main(String[] args) {
         DVD[] dvdCollection = new DVD[15];
-        
+
+        // Writing items into the array
+        DVD avengersDVD = new DVD("The Avengers", 2012, "Joss Whedon");
+        DVD incrediblesDVD = new DVD("The Incredibles", 2004, "Brad Bird");
+        DVD findingDoryDVD = new DVD("Finding Dory", 2016, "Andrew Stanton");
+        DVD lionKingDVD = new DVD("The Lion King", 2019, "Jon Favreau");
+        dvdCollection[7] = avengersDVD;
+        dvdCollection[3] = incrediblesDVD;
+        dvdCollection[9] = findingDoryDVD;
+        dvdCollection[2] = lionKingDVD;
+
+        // Reading items in the array
+        System.out.println(dvdCollection[7]); // The Avengers, directed by Joss Whedon, released in 2012
+        System.out.println(dvdCollection[10]); //null
+        System.out.println(dvdCollection[3]); //The Incredibles, directed by Brad Bird, released in 2004
     }
 }
 
