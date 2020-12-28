85f4fd136fa71d934c69f46fb2cf4621f3bea98b
Christopher Guerrero <chrisgrr3@gmail.com>
2020-12-27 20:51:45 -0900

    Create mergeSorted.js
    This function merges two sorted arrays into one new array.

---
+++ ArrayAlgos/mergeSorted.js
@@ -0,0 +1,20 @@
+const nums1 = [1,2,3];
+const nums2 = [2,5,6];
+
+var mergeSorted = (f,s) => {
+    let newArr = [];
+    fIndex = 0;
+    sIndex = 0;
+    while(newArr.length != f.length + s.length) {
+        if(f[fIndex] < s[sIndex]) {
+            newArr.push(f[fIndex]);
+            fIndex++;
+        } else {
+            newArr.push(s[sIndex]);
+            sIndex++;
+        }
+    }
+    return newArr;
+}
+
+console.log(mergeSorted(nums1, nums2));
